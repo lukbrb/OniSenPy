f9c476bf698c3b1cd08f5deaa6f369a75c23315e
lucas <lucasbarbr@outlook.com>
2022-01-03 20:22:19 +0100

    Correction setup

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     url='',
     license='GNU AGPLv3 ',
     author='Lucas Barbier',
-    author_email='lbge.dev@free.fr',
+    author_email='lucas.barbier@protonmail.com',
     description='Bibliothèque Python servant d\'interface Python à l\'API de l\'ONISEP (http://opendata.onisep.fr/3-api.htm)'
 )
